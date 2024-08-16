edb81982e4a40213ed588bf9f09a4f284c2ac8d2
Mikiman1964 <116963826+Mikiman1964@users.noreply.github.com>
2024-08-16 18:35:38 +0200

    Delete yellowdrop.h

--- yellowdrop.h
+++
@@ -1,9 +0,0 @@
-#ifndef YELLOWDROP_H_INCLUDED
-#define YELLOWDROP_H_INCLUDED
-
-void ControlYellowDrop(uint32_t I);
-void ControlYellowCheese(uint32_t I);
-void ControlSpreadYellowCheese(uint32_t I);
-bool IsYellowCheeseAround(uint32_t t);
-
-#endif // YELLOWDROP_H_INCLUDED
