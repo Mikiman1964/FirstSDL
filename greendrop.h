5fda04ec3eb3f4c52ff30f9e2f8e4fba04f7ca73
Mikiman1964 <116963826+Mikiman1964@users.noreply.github.com>
2024-08-16 18:30:02 +0200

    Delete greendrop.h

--- greendrop.h
+++
@@ -1,9 +0,0 @@
-#ifndef GREENDROP_H_INCLUDED
-#define GREENDROP_H_INCLUDED
-
-void ControlGreenDrop(uint32_t I);
-void ControlGreenCheese(uint32_t I);
-void ControlSpreadGreenCheese(uint32_t I);
-bool IsGreenCheeseAround(uint32_t t);
-
-#endif // GREENDROP_H_INCLUDED
